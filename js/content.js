9f0b6598819dd783b42a271c02b2cfe0033a14c3
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2020-06-08 18:48:37 -0500

    Extend the functionality to the links

--- js/content.js
+++ js/content.js
@@ -1,71 +1,91 @@
 const badges = ['primary', 'success', 'danger', 'warning', 'info'];
 
 $(document).ready(function() {
-    $('.button-link').on('click', function() {
-        let intervalWait = setInterval(() => {
+    $('.button-link').on('click', attachEvents);
+    let dataFound = $('.section-list').length;
+    if (dataFound) {
+        attachEvents();
+    }
+});
 
-            let dataNotFound = $('.zeromatches').length
-            let dataFound = $('.section-list').length
+attachEvents = function() {
+    let intervalWait = setInterval(() => {
 
-            if (dataFound) {
-                $('div.section-list').find("tr").each(function() {
-                    $(this).on("click", function() {
+        let dataNotFound = $('.zeromatches').length
+        let dataFound = $('.section-list').length
 
-                        let child_row = '#' + $(this).attr('id') + 'child';
-                        let child_content = child_row + 'content';
-                        let data_id = $(this).attr('data-id').replace(/\./g, '-');
-                        let condition = $(child_row).length > 0 ? true : false;
+        if (dataFound) {
+            $('div.section-list').find("tr").each(function() {
+                $(this).on("click", function() {
 
-                        if (condition) {
-                            let profName = $(this).find('td:nth-child(4)').find('a').text();
-                            let courseName = $(this).find('td:nth-child(2)').find('a').text();
+                    let child_row = '#' + $(this).attr('id') + 'child';
+                    let child_content = child_row + 'content';
+                    let data_id = $(this).attr('data-id').replace(/\./g, '-');
+                    let condition = $(child_row).length > 0 ? true : false;
 
-                            profName = profName.substring(0, profName.indexOf(' ')) + ' ' + profName.substring(profName.lastIndexOf(' ') + 1);
+                    if (condition) {
+                        let profName = $(this).find('td:nth-child(4)').find('a').text();
+                        let courseName = $(this).find('td:nth-child(2)').find('a').text();
 
-                            chrome.runtime.sendMessage({
-                                request: "get",
-                                id: data_id,
-                                body: {
-                                    professor_name: profName,
-                                    course_name: courseName
-                                }
-                            }, function(response) {
-                                if (response.response == 'data') {
+                        profName = profName.substring(0, profName.indexOf(' ')) + ' ' + profName.substring(profName.lastIndexOf(' ') + 1);
 
-                                    let waitTime = setInterval(() => {
-                                        let visibility = $(child_content).find('.courseinfo__overviewtable').length;
-                                        if (visibility) {
-                                            $.get(chrome.runtime.getURL('/templates/template.html'), function(data) {
-                                                result = ejs.render(data, response);
-                                                element = $(child_content).find('tbody tr:nth-child(4)').eq(1);
-                                                $(result).insertAfter(element);
-                                            });
-                                            clearInterval(waitTime);
-                                        }
-                                    }, 200);
+                        chrome.runtime.sendMessage({
+                            request: "get",
+                            id: data_id,
+                            body: {
+                                professor_name: profName,
+                                course_name: courseName
+                            }
+                        }, function(response) {
+                            if (response.response == 'data') {
+                                console.log(response);
+                                let waitTime = setInterval(() => {
+                                    let visibility = $(child_content).find('.courseinfo__overviewtable').length;
+                                    if (visibility) {
+                                        $.get(chrome.runtime.getURL('/templates/template.html'), function(data) {
+                                            result = ejs.render(data, response);
+                                            element = $(child_content).find('tbody tr:nth-child(4)').eq(1);
+                                            $(result).insertAfter(element);
+                                        });
+                                        clearInterval(waitTime);
+                                    }
+                                }, 200);
 
-                                } else {
-                                    let waitTime = setInterval(() => {
-                                        let visibility = $(child_content).find('.courseinfo__overviewtable').length;
-                                        if (visibility) {
-                                            $.get(chrome.runtime.getURL('/templates/error.html'), function(data) {
-                                                result = ejs.render(data, response);
-                                                element = $(child_content).find('tbody tr:nth-child(4)').eq(1);
-                                                $(result).insertAfter(element);
-                                            });
-                                            clearInterval(waitTime);
-                                        }
-                                    }, 200);
-                                }
-                            });
+                            } else {
+                                let waitTime = setInterval(() => {
+                                    let visibility = $(child_content).find('.courseinfo__overviewtable').length;
+                                    if (visibility) {
+                                        $.get(chrome.runtime.getURL('/templates/error.html'), function(data) {
+                                            result = ejs.render(data, response);
+                                            element = $(child_content).find('tbody tr:nth-child(4)').eq(1);
+                                            $(result).insertAfter(element);
+                                        });
+                                        clearInterval(waitTime);
+                                    }
+                                }, 200);
+                            }
+                        });
 
-                        }
+                    }
+                });
+
+                $(this).find("td a").each(function() {
+                    $(this).on('click', function() {
+                        setTimeout(function() {
+                            $(window).load(attachEvents);
+                        }, 2000);
+                    })
+                });
+
+                $(this).find("td.actions div").each(function() {
+                    $(this).on('click', function() {
+                        console.log("I am clicked");
                     });
                 });
-                clearInterval(intervalWait);
-            } else if (dataNotFound) {
-                clearInterval(intervalWait);
-            }
-        }, 1000);
-    });
-});
+            });
+            clearInterval(intervalWait);
+        } else if (dataNotFound) {
+            clearInterval(intervalWait);
+        }
+    }, 1000);
+}
